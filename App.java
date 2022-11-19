7807938e8dffe5c0672c900bbf3dd77baf91a21e
tallesweiler <talles.cavalleiro@gmail.com>
2022-11-19 01:14:07 -0300

    att cod

--- App.java
+++ App.java
@@ -15,7 +15,7 @@ public class App {
 
     //números 95, 96, 97, 98 representam casos de votos em branco, nulos ou anulados
 
-    public static void geraArquivoDeputados(String nomeArquivo, String sigla) throws FileNotFoundException, IOException {
+    public static void geraArquivoDeputados(String nomeArquivo, String sigla, LinkedList<Politico> deputadosFederais, LinkedList<Politico> deputadosEstaduais) throws FileNotFoundException, IOException {
         try(FileInputStream in = new FileInputStream(nomeArquivo);
             Reader r = new InputStreamReader(in, "ISO-8859-1");
             BufferedReader br = new BufferedReader(r);) 
@@ -24,9 +24,6 @@ public class App {
 
             String linha=br.readLine();
             linha=br.readLine();
-
-            LinkedList<Politico> deputadosFederais = new LinkedList<Politico>();
-            LinkedList<Politico> deputadosEstaduais = new LinkedList<Politico>();
 
             while(linha!=null) {
                 try (Scanner scan = new Scanner(linha)) {
@@ -46,12 +43,11 @@ public class App {
         }
     }
 
-    public static void geraArquivoVotos(String nomeArquivo, String sigla) throws FileNotFoundException, IOException {
+    public static void geraArquivoVotos(String nomeArquivo, String sigla, LinkedList<Politico> deputadosFederais, LinkedList<Politico> deputadosEstaduais) throws FileNotFoundException, IOException {
         try(FileInputStream in = new FileInputStream(nomeArquivo);
             Reader r = new InputStreamReader(in, "ISO-8859-1");
             BufferedReader br = new BufferedReader(r);) 
         {
-            Politico politico=new Politico(11122);
             BufferedWriter buffWrite = new BufferedWriter(new FileWriter("testesVotos/teste"+sigla+".txt"));
             
             String linha=br.readLine();
@@ -59,18 +55,39 @@ public class App {
 
             while(linha!=null) {
                 try (Scanner scan = new Scanner(linha)) {
-                    buffWrite.append(fazLinhaVotos(scan, politico) + "\n");
+                    String line=fazLinhaVotos(scan, deputadosFederais, deputadosEstaduais);
+                    if (line!=null)
+                        buffWrite.append(line+"\n");
                     linha=br.readLine();
                 } catch(NoSuchElementException e) {
                     System.out.println("aa");
                 }
             }
-            System.out.println(politico.getQuantidadeDeVotos());
             buffWrite.close();
         } catch (UnsupportedEncodingException e) {
             System.out.println("bb");
         } catch (IOException e) {
             System.out.println("cc");
+        }
+    }
+
+    public static void geraArquivoVotosDeputados(String sigla, LinkedList<Politico> deputadosFederais, LinkedList<Politico> deputadosEstaduais) throws IOException{
+        try {
+            BufferedWriter buffWrite = new BufferedWriter(new FileWriter("testesVotosDeputados/testeFederais"+sigla+".txt"));
+            BufferedWriter buffeWrite = new BufferedWriter(new FileWriter("testesVotosDeputados/testeEstaduais"+sigla+".txt"));
+
+            for (Politico p : deputadosFederais) {
+                buffWrite.append(p+"\n");
+            }
+
+            for (Politico p : deputadosEstaduais) {
+                buffeWrite.append(p+"\n");
+            }
+
+            buffWrite.close();
+            buffeWrite.close();
+        } catch (IOException e) {
+            System.out.println("aaa");
         }
     }
 
@@ -153,6 +170,9 @@ public class App {
         passaDireto(1, scan);
         CD_SITUACAO_CANDIDATO_TOT=scan.nextInt();
         scan.nextLine();
+        if (CD_SITUACAO_CANDIDATO_TOT!=2 && CD_SITUACAO_CANDIDATO_TOT!=16) {
+            return null;
+        }
 
         if (CD_CARGO==6) {
             Politico politico=new Politico(CD_CARGO,NR_CANDIDATO,NM_URNA_CANDIDATO,NR_PARTIDO,SG_PARTIDO,NR_FEDERACAO,DT_NASCIMENTO,CD_GENERO,CD_SIT_TOT_TURNO,NM_TIPO_DESTINACAO_VOTOS,CD_SITUACAO_CANDIDATO_TOT);
@@ -168,7 +188,7 @@ public class App {
         return null;
     }
 
-    public static String fazLinhaVotos(Scanner scan, Politico politico) {
+    public static String fazLinhaVotos(Scanner scan, LinkedList<Politico> deputadosFederais, LinkedList<Politico> deputadosEstaduais) {
         int CD_CARGO;    //6 para deputado federal e 7 para deputado estadual;
         int NR_VOTAVEL;  //o número do candidato no caso de voto nominal ou o número do partido se for voto na legenda. Os números 95, 96, 97, 98 representam casos de votos em branco, nulos ou anulados, e, como só estamos nos concentrando nos votos válidos, linhas com esses números devem ser ignoradas. Atenção, pois há casos em que NR_VOTAVEL é um número de candidato, porém este candidato tem seus votos nominais redirecionados para a legenda como indicado acima para NM_TIPO_DESTINACAO_VOTOS no arquivo de candidatos igual a “Válido (legenda)”
         int QT_VOTOS;    //número de votos (no candidato ou no partido) na sessão.
@@ -178,6 +198,10 @@ public class App {
         scan.useDelimiter("\"");
         passaDireto(1, scan);
         CD_CARGO=scan.nextInt();
+        if (CD_CARGO!=6 && CD_CARGO!=7) {
+            scan.nextLine();
+            return null;
+        }
         scan.useDelimiter(";");
         passaDireto(2, scan);
         scan.useDelimiter("\"");
@@ -192,18 +216,37 @@ public class App {
         passaDireto(4, scan);
         scan.nextLine();
 
-        if (NR_VOTAVEL==politico.getNumeroDoCandidato())
-            politico.adicionaVotos(QT_VOTOS);
-
-        return CD_CARGO+";"+NR_VOTAVEL+";"+QT_VOTOS+";";
+        if (CD_CARGO==6) {
+            for (Politico p : deputadosFederais) {
+                if (p.getNumeroDoCandidato()==NR_VOTAVEL) {
+                    p.adicionaVotos(QT_VOTOS);
+                    break;
+                }     
+            }
+            return ";"+CD_CARGO+";"+NR_VOTAVEL+";"+QT_VOTOS+";";
+        }
+        else if (CD_CARGO==7) {
+            for (Politico p : deputadosEstaduais) {
+                if (p.getNumeroDoCandidato()==NR_VOTAVEL) {
+                    p.adicionaVotos(QT_VOTOS);
+                    break;
+                }  
+            }
+            return ";"+CD_CARGO+";"+NR_VOTAVEL+";"+QT_VOTOS+";";
+        }
+        
+        return null;
     }
 
     public static void main(String[] args) throws IOException, FileNotFoundException {
         try {
             Scanner scan = new Scanner(System.in);
             String sigla=scan.nextLine();
-            geraArquivoDeputados("consulta_cand_2022/consulta_cand_2022_"+sigla+".csv", sigla);
-            geraArquivoVotos("arquivos_de_votacao/votacao_secao_2022_"+sigla+".csv", sigla);
+            LinkedList<Politico> deputadosFederais = new LinkedList<Politico>();
+            LinkedList<Politico> deputadosEstaduais = new LinkedList<Politico>();
+            geraArquivoDeputados("consulta_cand_2022/consulta_cand_2022_"+sigla+".csv", sigla, deputadosFederais, deputadosEstaduais);
+            geraArquivoVotos("arquivos_de_votacao/votacao_secao_2022_"+sigla+".csv", sigla, deputadosFederais, deputadosEstaduais);
+            geraArquivoVotosDeputados(sigla, deputadosFederais, deputadosEstaduais);
             scan.close();
         }
         catch (FileNotFoundException e) {
