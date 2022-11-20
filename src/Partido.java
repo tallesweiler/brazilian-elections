fda2ab88ca6396a6156a7b09f9b33922552760eb
jvmoraiscb <jv.moraiscb@gmail.com>
2022-11-19 22:16:33 -0300

    :heavy_check_mark: armazena todos os valores corretamente

--- src/Partido.java
+++ src/Partido.java
@@ -1,50 +1,66 @@
 package src;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Partido {
     private int numeroDoPartido; // número do partido;
     private String siglaDoPartido; // sigla do partido;
     private int quantidadeDeVotos;
-    private List<Politico> politicos;
+    private Map<Integer, Deputado> deputados;
 
+    
     public Partido(int numeroDoPartido, String siglaDoPartido) {
         this.numeroDoPartido = numeroDoPartido;
         this.siglaDoPartido = siglaDoPartido;
+        this.quantidadeDeVotos = 0;
+        this.deputados = new HashMap<>();
     }
-
     public Partido() {
     }
-
+    
     public int getNumeroDoPartido() {
         return numeroDoPartido;
     }
-
     public void setNumeroDoPartido(int numeroDoPartido) {
         this.numeroDoPartido = numeroDoPartido;
     }
-
     public String getSiglaDoPartido() {
         return siglaDoPartido;
     }
-
     public void setSiglaDoPartido(String siglaDoPartido) {
         this.siglaDoPartido = siglaDoPartido;
     }
-
     public int getQuantidadeDeVotos() {
         return quantidadeDeVotos;
     }
-
     public void setQuantidadeDeVotos(int quantidadeDeVotos) {
         this.quantidadeDeVotos = quantidadeDeVotos;
     }
-
-    public List<Politico> getPoliticos() {
-        return politicos;
+    public Map<Integer, Deputado> getDeputados() {
+        return deputados;
+    }
+    public void setDeputados(Map<Integer, Deputado> deputados){
+        this.deputados = deputados;
     }
 
-    public void setPoliticos(List<Politico> politicos) {
-        this.politicos = politicos;
+    public Deputado retornaDeputado(int id){
+        return deputados.get(id);
+    }
+    public void adicionaDeputado(int id, Deputado deputado) {
+        this.deputados.put(id, deputado);
+    }
+    public void adicionaVotos(int valor) {
+        this.setQuantidadeDeVotos(this.getQuantidadeDeVotos() + valor);
+    }
+
+    public String toString(){
+        String string;
+        string = siglaDoPartido + "(" + quantidadeDeVotos + ")\n";
+        for(Deputado d : deputados.values()){
+            string += d + "\n";
+        }
+
+        return string;
     }
 }
